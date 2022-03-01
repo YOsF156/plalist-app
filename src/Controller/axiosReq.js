f0c677e3844b3c068ca29abdb8328037fad54295
YOsF156 <yosistar@gmail.com>
2022-03-01 20:39:12 +0200

    deploy

--- src/Controller/axiosReq.js
+++ src/Controller/axiosReq.js
@@ -4,6 +4,6 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "https://ys-playlist-server.herokuapp.com/",
 });
-localStorage.PLaccessToken ? api.defaults.headers.common["Authorization"] = `bearer ${localStorage.PLaccessToken}` : alert("sdd")
+localStorage.PLaccessToken ? api.defaults.headers.common["Authorization"] = `bearer ${localStorage.PLaccessToken}` : console.log("you need to login")
 
 export default api;
