d61501ce457611b5e7a3257ff13d05da459414d0
yosef sharvit <92664347+YOsF156@users.noreply.github.com>
2022-05-09 14:28:25 +0300

    Update axiosReq.js

--- src/Controller/axiosReq.js
+++ src/Controller/axiosReq.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://ys-playlist-server.herokuapp.com/ ",
+    baseURL: "https://ys-playlist-server.herokuapp.com/",
 });
 localStorage.PLaccessToken ? api.defaults.headers.common["Authorization"] = `bearer ${localStorage.PLaccessToken}` : console.log("you need to login")
 
